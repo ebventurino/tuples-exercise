75a2fff302d0f587f15a5fc04772e70f80e92445
Emily <ebventurino@gmail.com>
2018-08-29 09:31:36 -0500

    added comments

--- Program.cs
+++ Program.cs
@@ -5,8 +5,10 @@ namespace tuples
 {
     class Program
     {
+        //executing the main code
         static void Main(string[] args)
         {
+            // creating a list that will contain a string, decimal, and int
             List<(string product, double amount, int quantity)> transactions = new List<(string, double, int)>()
             {
                 ("apples", 22.50, 23),
@@ -15,12 +17,18 @@ namespace tuples
                 ("bananas", 31.50, 23),
                 ("peaches", 24.50, 23),
             };
+
+            // creating an empty int and decimal
             int totalProductsSold = 0;
             double TotalRevenue = 0;
+
+            // looping through the transactions
             foreach ((string product, double amount, int quantity) t in transactions)
 
             {
+                // total products sold = quantity added together
                 totalProductsSold += t.quantity;
+                // total revenue = total quantity times amount
                 TotalRevenue += t.quantity * t.amount;
 
             }
